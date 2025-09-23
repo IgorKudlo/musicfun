f7c08c3940f8377a9f7886c249a5b42622d64e63
igor.kudlo <igoreklida@gmail.com>
2025-09-23 18:50:01 +0300

    infinite scroll

--- rtk-query/src/features/tracks/ui/TracksPage.tsx
+++ rtk-query/src/features/tracks/ui/TracksPage.tsx
@@ -1,49 +1,21 @@
-import { useFetchTracksInfiniteQuery } from '../api/tracksApi.ts'
-import s from './TracksPage.module.css'
+import { useInfiniteScroll } from '@/common/hooks'
+import { useFetchTracksInfiniteQuery } from '../api/tracksApi'
+import { LoadingTrigger } from './LoadingTrigger/LoadingTrigger'
+import { TracksList } from './TracksList/TracksList'
 
 export const TracksPage = () => {
-  const { data, isLoading, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage } = useFetchTracksInfiniteQuery()
+  const { data, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage } = useFetchTracksInfiniteQuery()
 
-  const pages = data?.pages.map((page) => page.data).flat() || []
-  // const pages = data?.pages.flatMap((page) => page.data) || []
+  const { observerRef } = useInfiniteScroll({ hasNextPage, isFetching, fetchNextPage })
 
-  const loadMoreHandler = () => {
-    if (hasNextPage && !isFetching) {
-      fetchNextPage()
-    }
-  }
+  const pages = data?.pages.flatMap((page) => page.data) || []
 
   return (
     <div>
       <h1>Tracks page</h1>
-
-      <div className={s.list}>
-        {pages.map((track) => {
-          const { title, user, attachments } = track.attributes
-
-          return (
-            <div key={track.id} className={s.item}>
-              <div>
-                <p>Title: {title}</p>
-                <p>Name: {user.name}</p>
-              </div>
-              {attachments.length ? <audio controls src={attachments[0].url} /> : 'no file'}
-            </div>
-          )
-        })}
-      </div>
-
-      {!isLoading && (
-        <>
-          {hasNextPage ? (
-            <button onClick={loadMoreHandler} disabled={isFetching}>
-              {isFetchingNextPage ? 'Loading...' : 'Load More'}
-            </button>
-          ) : (
-            <p>Nothing more to load</p>
-          )}
-        </>
-      )}
+      <TracksList tracks={pages} />
+      {hasNextPage && <LoadingTrigger isFetchingNextPage={isFetchingNextPage} observerRef={observerRef} />}
+      {!hasNextPage && pages.length > 0 && <p>Nothing more to load</p>}
     </div>
   )
 }
